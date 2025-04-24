8b7155499b88863eaff8b3b27ca794010a340e57
Costa <117647243+Jaca7x@users.noreply.github.com>
2025-04-24 18:03:12 -0300

    Update game.c

--- AimLab/aimlab/game.c
+++ AimLab/aimlab/game.c
@@ -6,6 +6,8 @@
 
 static Rectangle alvo[MAX_ALVO];
 static int points = 0;
+static float time = 10.0f;
+static bool timeUp = false;
 static int screenWidth, screenHeight;
 
 static void repositionRectangle(int i) {
@@ -27,6 +29,7 @@ void InitGame(int sw, int sh) {
 void UpdateGame(void) {
     Vector2 mouse = GetMousePosition();
 
+
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
         for (int i = 0; i < MAX_ALVO; i++) {
             if (CheckCollisionPointRec(mouse, alvo[i])) {
@@ -35,16 +38,38 @@ void UpdateGame(void) {
             }
         }
     }
+
+    if (!timeUp)
+    {
+        time -= GetFrameTime();
+        if (time <= 0.0f)
+        {
+            time = 0.0f;
+            timeUp = true;
+        }
+        
+    }
+    
+
 }
 
 void DrawGame(void) {
     BeginDrawing();
     ClearBackground(BLACK);
 
+    if (timeUp)
+    {
+        ClearBackground(WHITE);
+        DrawText(TextFormat("O TEMPO ACABOU, SEUS PONTOS FORAM: %d", points), 50, 70, 30, RED);
+    } else {
+        ClearBackground(BLACK);
+    }
+
     for (int i = 0; i < MAX_ALVO; i++) {
         DrawRectangleRec(alvo[i], WHITE);
     }
 
     DrawText(TextFormat("Points: %d", points), 10, 10, 20, RED);
+    DrawText(TextFormat("%.2f", time), 350, 10, 40, RED);
     EndDrawing();
 }
